d9c884b42f0eabca9553ae11c7bca504a4ffa798
devika <devikavv@xminds.com>
2025-10-15 14:23:50 +0530

    added notfound page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: '**', redirectTo: '/home' } // Wildcard route for 404 page
+  { path: '**', component: NotFoundComponent } // Wildcard route for 404 page
 ];

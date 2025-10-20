5e5ed2ccda8e32d845b95b31682d7fbb3c4ffd65
devika <devikavv@xminds.com>
2025-10-20 16:14:13 +0530

    Basic pipes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { BasicPipesComponent } from './basic-pipes/basic-pipes.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AuthGuard } from './auth.guard';
 import { UnsavedChangesGuard } from './unsaved-changes.guard';
@@ -13,5 +14,6 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
   { path: 'edit-profile', component: EditProfileComponent, canActivate: [AuthGuard], canDeactivate: [UnsavedChangesGuard] },
+  { path: 'basic-pipes', component: BasicPipesComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent } // Wildcard route for 404 page
 ];
---
+++ src/app/basic-pipes/basic-pipes.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-basic-pipes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './basic-pipes.component.html',
+  styleUrl: './basic-pipes.component.css'
+})
+export class BasicPipesComponent {
+  price: number = 12345.6789;
+  currentDate: Date = new Date();
+  myObject: any = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com'
+  };
+  myString: string = 'This is a STRING in Mixed CASE';
+  myNumber: number = 0.23;
+  myArray: any[] = ['apple', 'banana', 'orange', 'grape', 'mango'];
+  longString: string = 'This is a long string.';
+  titleString: string = 'this is a sentence in lowercase.';
+  myObservable$: Observable<number> = of(42);
+  myPromise$: Promise<string> = Promise.resolve('Hello World!');
+}

077eb4d6fcd1602d8eff5962a32b15e82b4b2d8a
devika <devikavv@xminds.com>
2025-10-20 17:34:29 +0530

    custom pipe

--- src/app/basic-pipes/basic-pipes.component.ts
+++ src/app/basic-pipes/basic-pipes.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
+import { ReversePipe } from '../custom-pipe.pipe';
 
 @Component({
   selector: 'app-basic-pipes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReversePipe],
   templateUrl: './basic-pipes.component.html',
   styleUrl: './basic-pipes.component.css'
 })
@@ -22,6 +23,7 @@ export class BasicPipesComponent {
   myArray: any[] = ['apple', 'banana', 'orange', 'grape', 'mango'];
   longString: string = 'This is a long string.';
   titleString: string = 'this is a sentence in lowercase.';
+  reverseString: string = 'Hello Angular!';
   myObservable$: Observable<number> = of(42);
   myPromise$: Promise<string> = Promise.resolve('Hello World!');
 }
